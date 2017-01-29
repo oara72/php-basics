1cbf193001c3b40fe7e8abd1e9f40f6506eeda53
oliver alexander <rodriguezarrocha@gmail.com>
2017-01-29 07:32:40 -0500

    crud

---
+++ w4_php_crud/b_insert/connectvars.php
@@ -0,0 +1,7 @@
+<?php
+//define the Database connection constant here!
+define('DB_HOST', 'localhost');
+define('DB_USER', 'w0668948');
+define ('DB_PASSWORD','gr5egr5e');
+define ('DB_NAME', 'w0668948db');
+?>
---
+++ w4_php_crud/b_insert/movieInsert.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Insert Form - Week 4 - by Oliver Rodriguez</title>
+</head>
+<body>
+<?php
+$author = 'Oliver Rodriguez';
+$Title = trim($_POST['Title']);
+$ProductionCompany = trim($_POST['ProductionCompany']);
+$YearReleased = trim($_POST['YearReleased']);
+$Director = trim($_POST['Director']);
+
+//set the variables for the Database acces;
+require_once('connectvars.php');
+
+//connect to the database
+$dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+//DATA from MySQL
+$query = "INSERT into Movies values ('0', '$Title', '$ProductionCompany', '$YearReleased', '$Director')";
+
+print("The query is: <br>$query<br><br>");
+
+if (mysqli_query($dbc, $query)) {
+	print ("The query was successfully executed !<br/>");
+
+} else {
+	print ("the query could not be executed <br>");
+}
+mysqli_close($dbc);
+
+?>
+</body>
+</html>
