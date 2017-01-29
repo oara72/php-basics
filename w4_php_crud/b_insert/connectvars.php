621d67ef287183c101acdfd13ec07f23e3361e25
Oliver Alexander <rodriguezarrocha@gmail.com>
2017-01-29 07:48:14 -0500

    Delete connectvars.php

--- w4_php_crud/b_insert/connectvars.php
+++
@@ -1,7 +0,0 @@
-<?php
-//define the Database connection constant here!
-define('DB_HOST', 'localhost');
-define('DB_USER', 'w0668948');
-define ('DB_PASSWORD','gr5egr5e');
-define ('DB_NAME', 'w0668948db');
-?>
